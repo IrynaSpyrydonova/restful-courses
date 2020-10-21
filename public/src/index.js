bfc6fd5150fda6a1a15c64cd3921bc69c0efcd31
Iryna Spyrydonova <Iryna059@gmail.com>
2020-10-21 12:31:17 +0200

    Added POST methos
    Closes gh-3

--- public/src/index.js
+++ public/src/index.js
@@ -1,1 +1,53 @@
-console.log('hi');
+
+
+const Joi = require('joi');
+const express = require('express');
+const fs = require('fs');
+const app = express();
+
+app.use(express.json());
+
+// Path to the file
+COURSES_PATH =  __dirname + '/' + 'courses.json';
+
+
+
+// POST METHOD
+app.post('/api/courses', (req, res)=>{
+    const { error } = validateCourse(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+    
+    fs.readFile(COURSES_PATH, 'utf-8', (err, data) => {
+        if (err)  return res.status(500).send(err.message);
+        let courses = JSON.parse(data);
+        console.log(courses);
+        const course = {
+            id: courses.length + 1,
+            name: req.body.name
+        };
+        courses.push(course);
+        res.send(course);
+        let newData = JSON.stringify(courses, null, 2);
+        
+        fs.writeFile(COURSES_PATH, newData, (err) => {
+            if (err) return res.status(500).send(err.message);
+            console.log('Data written to file');
+        });
+    });
+});
+
+
+
+
+
+// VALIDATION
+function validateCourse(course){
+    const schema = Joi.object({ name: Joi.string() .min(3) .required() });
+    return schema.validate(course);
+}
+
+
+
+// Port
+const port = process.env.PORT || 3000;
+app.listen(port, ()=> console.log(`Listening on port ${port} ...`));
